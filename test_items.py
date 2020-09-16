051a179cff6607b10bbbc5984af7c451eb3cae42
sever-t <sever.74@list.ru>
2020-09-16 12:56:16 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,9 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_order_basket(browser):
     browser.get(link)
-	# browser.implicitly_wait(4)   # время ожидания загрузки элементов страницы как альтернатива time.sleep
     time.sleep(30)
     button = browser.find_element_by_class_name('btn-add-to-basket')
     assert len(button.text) > 0, 'Кнопка добавления товара в корзину отсутсвует'
-	# assert button != 'null', 'Кнопка добавления товара в корзину отсутсвует'  # еще вариант assert
-	# assert button.is_displayed(), 'Кнопка добавления товара в корзину отсутсвует'  # еще вариант assert
+	
